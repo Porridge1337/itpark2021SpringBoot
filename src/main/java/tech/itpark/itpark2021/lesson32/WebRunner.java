9405fd43b713f875f4dc8cb17cc478440ce27e2e
Yatsukin <79377539+Yatsukin@users.noreply.github.com>
2022-03-12 03:22:47 +0300

    Выполнение 32-го задания

---
+++ src/main/java/tech/itpark/itpark2021/lesson32/WebRunner.java
@@ -0,0 +1,13 @@
+package tech.itpark.itpark2021.lesson32;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebRunner.class, args);
+    }
+
+}
