3f35ca3badd887c6c8e2f473614392bd1eb33a01
Yatsukin <79377539+Yatsukin@users.noreply.github.com>
2022-04-07 19:00:30 +0300

    Выполнение 37-го задания

--- basic/src/main/java/tech/itpark/itpark2021/lesson35/model/Role.java
+++ basic/src/main/java/tech/itpark/itpark2021/lesson35/model/Role.java
@@ -11,7 +11,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 import java.util.List;
