2cd3d8f4455c99475a62f89da6aecdc521f5a8a0
Yatsukin <79377539+Yatsukin@users.noreply.github.com>
2022-04-21 02:26:52 +0300

    Выполнение 39-го задания (мелкие правки)

--- test-module/src/main/java/lesson39/controller/RestTemplateAopController.java
+++ test-module/src/main/java/lesson39/controller/RestTemplateAopController.java
@@ -2,15 +2,19 @@ package lesson39.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.validation.constraints.NotNull;
 import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
+@Validated
 public class RestTemplateAopController {
 
     private final RestTemplate restTemplate;
@@ -27,7 +31,7 @@ public class RestTemplateAopController {
     }
 
     @GetMapping("/city")
-    public String getCity(String name) {
+    public String getCity(@RequestParam @NotNull String name) {
         return restTemplate.getForObject(UriComponentsBuilder.newInstance()
                 .scheme("http")
                 .host("localhost")
