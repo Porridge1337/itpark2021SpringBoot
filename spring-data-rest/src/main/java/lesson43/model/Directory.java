666611e0f340e89b17a6f0acbce591ccf4443e20
Yatsukin <79377539+Yatsukin@users.noreply.github.com>
2022-04-26 02:20:39 +0300

    Выполнение 43-го задания

--- spring-data-rest/src/main/java/lesson43/model/Directory.java
+++ spring-data-rest/src/main/java/lesson43/model/Directory.java
@@ -11,7 +11,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
