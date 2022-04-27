afedf148c91ddbb7c3c67e33563bd38f92523bc3
Yatsukin <79377539+Yatsukin@users.noreply.github.com>
2022-04-27 15:23:38 +0300

    Выполнение 43-го задания

--- spring-data-rest/src/main/java/lesson43/controller/DirectoryController.java
+++ spring-data-rest/src/main/java/lesson43/controller/DirectoryController.java
@@ -6,6 +6,7 @@ import lesson43.service.DirectoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import javax.validation.Valid;
 
 @Controller
+@Validated
 @RequiredArgsConstructor
 public class DirectoryController {
 
